594843e43d1e9e18e4841a6a6b8a44491ab854bf
BAUTISTA <bautistajoaquin009@gmail.com>
2025-10-16 16:01:33 -0300

    segundo commit - suma

--- bauti.java
+++ bauti.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class bauti {
     public static void main(String[] args) {
-        System.out.println("hola mundo");
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Ingrese el primer número: ");
+        int a = sc.nextInt();
+        System.out.print("Ingrese el segundo número: ");
+        int b = sc.nextInt();
+        System.out.println("La suma es: " + (a + b));
+        sc.close();
     }
 }
